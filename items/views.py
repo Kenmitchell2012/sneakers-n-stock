c6269d1566622248618c748d8c446054b6e5e6ae
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2024-06-22 20:31:15 -0500

    add item page

--- items/views.py
+++ items/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Items
 # Create your views here.
+
+
+
+
+def items(request):
+    items = Items.objects.filter(is_sold=False)
+    return render(request, 'item/items.html', {'items': items})
+
 
 def detail(request, pk):
     item = get_object_or_404(Items, pk=pk)
@@ -24,6 +32,19 @@ def new(request):
     else:
         form = NewItemForm
     return render(request, 'item/form.html', {'form': form,'title': 'New Item'},)
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Items, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {'form': form,'title': 'Edit Item'},)
 
 @login_required
 def delete(request, pk):
