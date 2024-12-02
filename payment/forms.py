2205104cc6a7bada322c4fef1629908eb569587c
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2024-12-01 19:29:46 -0600

    FIX- fix profile image width
    -fixed image width and height to show full image
    - changed homepage banner image/gif

--- payment/forms.py
+++ payment/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import ShippingAddress
 
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+
 class ShippingAddressForm(forms.ModelForm):
     class Meta:
         model = ShippingAddress
@@ -48,60 +52,90 @@ class ShippingAddressForm(forms.ModelForm):
     
 
 class PaymentForm(forms.Form):
-    card_name = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Name on Card',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_number = forms.CharField(widget=forms.NumberInput(attrs={
-        'placeholder': 'Card Number',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_exp_date = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Exp Date',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_cvv = forms.CharField(widget=forms.NumberInput(attrs={
-        'placeholder': 'CVV',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_address1 = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Address',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_address2 = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Address (optional)',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_city = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'City',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_state = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'State',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_zip_code = forms.CharField(widget=forms.NumberInput(attrs={
-        'placeholder': 'Zip Code',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_country = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Country',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
-    card_phone_number = forms.CharField(widget=forms.NumberInput(attrs={
-        'placeholder': 'Phone Number',
-        'class': 'w-full py-4 px-6 rounded-xl',
-        }))
-    
+    card_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Name on Card',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
 
+    card_number = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Card Number',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_exp_date = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Exp Date',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_cvv = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'CVV',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_address1 = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Address',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_address2 = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Address (optional)',
+            'class': INPUT_CLASSES,
+        }),
+    )
+
+    card_city = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'City',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_state = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'State',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_zip_code = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Zip Code',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_country = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Country',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
+
+    card_phone_number = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Phone Number',
+            'class': INPUT_CLASSES,
+        }),
+        error_messages={'required': ''},
+    )
 
